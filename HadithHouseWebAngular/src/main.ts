55a01ec727fea90b52f1e95af325d57e7625f4f2
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-03-04 15:18:44 -0800

    Add Facebook authentication

--- HadithHouseWebAngular/src/main.ts
+++ HadithHouseWebAngular/src/main.ts
@@ -1,12 +1,25 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import {initFbSdk} from './fbauth';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// Initialize FB SDK and then bootstrap angular. Notice that we bootstrap
+// even if we fail to load the SDK as we don't want the website not to
+// function if we cannot load the SDK, e.g. if there is an AD blocker in
+// the browser.
+initFbSdk(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+}, () => {
+  console.error('Failed to load FB SDK. This is most probably due to a ' +
+    'plugin in your browser, e.g. AdBlocker or Ghostery, blocking requests ' +
+    'to social websites. Disable blocking for this website and try again.');
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+});
