684f8beaf4d2e533e1db53d58536c7d527855adb
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-03-13 19:58:48 -0700

    Add home page and a template for not-found pages

--- HadithHouseWebAngular/src/app/app.module.ts
+++ HadithHouseWebAngular/src/app/app.module.ts
@@ -6,21 +6,21 @@ import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {FacebookService} from './facebook.service';
 import {HadithsComponent} from './hadiths/hadiths.component';
-import { BooksComponent } from './books/books.component';
-import { PersonsComponent } from './persons/persons.component';
-import { HadithTagsComponent } from './hadithtags/hadithtags.component';
-import { UsersComponent } from './users/users.component';
+import {BooksComponent} from './books/books.component';
+import {PersonsComponent} from './persons/persons.component';
+import {HadithTagsComponent} from './hadithtags/hadithtags.component';
+import {UsersComponent} from './users/users.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HomeComponent} from './home/home.component';
 
-/*
- * Use this later for invalid URLs.
-  { path: '**', component: PageNotFoundComponent }
- */
 const appRoutes: Routes = [
   {path: 'hadiths', component: HadithsComponent},
   {path: 'books', component: BooksComponent},
   {path: 'persons', component: PersonsComponent},
   {path: 'hadithtags', component: HadithTagsComponent},
-  {path: 'users', component: UsersComponent}
+  {path: 'users', component: UsersComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -30,7 +30,9 @@ const appRoutes: Routes = [
     BooksComponent,
     PersonsComponent,
     HadithTagsComponent,
-    UsersComponent
+    UsersComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
