d63d818e0643ca46f571e85077b86b85486448a2
Rafid Al-Humaimidi <rafidka@amazon.com>
2018-06-15 15:49:33 -0700

    Add tests for LoadingStatusService

--- src/app/core/loading-status.service.spec.ts
+++ src/app/core/loading-status.service.spec.ts
@@ -13,4 +13,27 @@ describe('LoadingStatusService', () => {
     (service: LoadingStatusService) => {
       expect(service).toBeTruthy();
     }));
+
+  it('pendingRequests is 0 initially', inject([LoadingStatusService],
+    (service: LoadingStatusService) => {
+      expect(service.pendingRequests).toBe(0);
+    }));
+
+  it('incrementPendingRequests() on 0 pending requests causes ' +
+    'isLoadingObservable() to get a true next value.',
+    inject([LoadingStatusService], (service: LoadingStatusService) => {
+      // Increment pending requests count and ensure isLoadingObservable()
+      // gets a true value.
+      service.incrementPendingRequests();
+      service.isLoadingObservable().subscribe(isLoading => {
+        expect(isLoading).toBe(true);
+      }).unsubscribe();
+
+      // Decrement pending requests count and ensure isLoadingObservable()
+      // gets a false value now.
+      service.decrementPendingRequests();
+      service.isLoadingObservable().subscribe(isLoading => {
+        expect(isLoading).toBe(false);
+      }).unsubscribe();
+    }));
 });
