0f021c574d0f6b031c6cc19efcc2ae17c8025da3
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-07-01 18:03:35 -0700

    Fix test in hadith.component.spec.ts

--- src/app/widgets/hadith/hadith.component.spec.ts
+++ src/app/widgets/hadith/hadith.component.spec.ts
@@ -1,21 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HadithComponent } from './hadith.component';
+import {HadithComponent} from './hadith.component';
+import {Hadith, HadithApiService} from '../../services/hadith-api.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
+import {of} from 'rxjs/internal/observable/of';
 
 describe('HadithComponent', () => {
   let component: HadithComponent;
   let fixture: ComponentFixture<HadithComponent>;
+  const HADITH_ID = 1;
+
+  function mockHadithApiService(): any {
+    const hadithApi = jasmine.createSpyObj('HadithApiService', ['get']);
+    hadithApi.get.and.returnValue(of(new Hadith()));
+    return hadithApi;
+  }
 
   beforeEach(async(() => {
+    mockHadithApiService();
+
     TestBed.configureTestingModule({
-      declarations: [ HadithComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        HadithComponent
+      ],
+      providers: [
+        {
+          provide: HadithApiService,
+          useValue: mockHadithApiService()
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HadithComponent);
     component = fixture.componentInstance;
+    component.hadithId = HADITH_ID;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
