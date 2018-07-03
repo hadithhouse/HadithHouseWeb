cf2b0f3f2c12743eed686ec9faf7b1a78e345290
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-07-02 18:48:17 -0700

    Commit _nav changes

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -5,6 +5,11 @@ export const navItems = [
     icon: 'icon-home',
   },
   {
+    name: 'Hadiths',
+    url: '/hadiths',
+    icon: 'icon-speech',
+  },
+  {
     name: 'Hadith Tags',
     url: '/hadithtags',
     icon: 'icon-tag',
